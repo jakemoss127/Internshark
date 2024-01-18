78aef131af28bbcbfb778d988f26ac1c92f8f6ff
jakemoss127 <jacobmoss@ufl.edu>
2024-01-18 13:29:19 -0500

    Added react-table

--- internship-portal/src/pages/Chart.jsx
+++ internship-portal/src/pages/Chart.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Nav from '../components/Nav.jsx';
+import './Chart.css';
+import DataTable from '../components/DataTable.jsx';
 
 const Chart = () => {
   return (
-    <div>
+    <div className='chart-container'>
         <Nav />
-        <h1>Chart</h1>
+        <h1 className='chart-title'>Internshark's Job Chart</h1>
+        <h2 className='subtitle'>Below is the list of internship listings to apply to</h2>
+        <div className="data-container">
+          <DataTable />
+        </div>
     </div>
   )
 }
