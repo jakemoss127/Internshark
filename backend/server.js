12b0c86fbafe75722b9a853a521a506dc9c7fb36
sethbehar <sethbehar@gmail.com>
2024-01-29 20:45:35 -0500

    Comments

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,9 @@ const options = {
     method: 'GET',
     url: 'https://jsearch.p.rapidapi.com/search',
     params: {
-        query: 'Economics',
+        query: 'Business',
         page: '1',
-        num_pages: '10',
+        num_pages: '20',
         date_posted: 'all',
         employment_types: 'INTERN',
     },
@@ -33,18 +33,18 @@ const options = {
     }
 };
 
-const fetchData = async () => {
-    try {
-        const response = await axios.request(options);
-        const dataToSave = JSON.stringify(response.data, null, 2);
-        fs.writeFileSync('fetchedData.json', dataToSave, 'utf-8');
-        console.log('Data saved to fetchedData.json');
+// const fetchData = async () => {
+//     try {
+//         const response = await axios.request(options);
+//         const dataToSave = JSON.stringify(response.data, null, 2);
+//         fs.writeFileSync('fetchedData.json', dataToSave, 'utf-8');
+//         console.log('Data saved to fetchedData.json');
 
-    } catch (error) {
-        console.error(error);
-    }
-    return;
-}
+//     } catch (error) {
+//         console.error(error);
+//     }
+//     return;
+// }
 
 // app.post('/add-jobs-software-engineering', async (req, res) => {
 //     try {
@@ -94,29 +94,29 @@ const fetchData = async () => {
 //     }
 // });
 
-app.post('/add-jobs-econ', async (req, res) => {
-    try {
-        // Read the dummy data JSON file
-        const data = JSON.parse(fs.readFileSync('fetchedData.json', 'utf8'));
+// app.post('/add-jobs-econ', async (req, res) => {
+//     try {
+//         // Read the dummy data JSON file
+//         const data = JSON.parse(fs.readFileSync('fetchedData.json', 'utf8'));
 
-        for (const job of data.data) {
-            const { employer_name, employer_website, job_title, job_apply_link, employer_logo, job_is_remote, job_city, job_state, job_posted_at_datetime_utc } = job;
+//         for (const job of data.data) {
+//             const { employer_name, employer_website, job_title, job_apply_link, employer_logo, job_is_remote, job_city, job_state, job_posted_at_datetime_utc } = job;
 
-            const existingJob = await pool.query('SELECT * FROM "Econ" WHERE employer_name = $1 AND job_title = $2',
-                [employer_name, job_title]);
+//             const existingJob = await pool.query('SELECT * FROM "Econ" WHERE employer_name = $1 AND job_title = $2',
+//                 [employer_name, job_title]);
 
-            // If the job doesn't exist, insert it into the database
-            if (existingJob.rows.length === 0) {
-                await pool.query('INSERT INTO "Econ" (employer_name, employer_website, job_title, job_apply_link, employer_logo, job_is_remote, job_city, job_state, job_posted_at_datetime_utc) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-                    [employer_name, employer_website, job_title, job_apply_link, employer_logo, job_is_remote, job_city, job_state, job_posted_at_datetime_utc]);
-            }
-        }
-        res.status(200).send('Jobs saved successfully');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
-    }
-});
+//             // If the job doesn't exist, insert it into the database
+//             if (existingJob.rows.length === 0) {
+//                 await pool.query('INSERT INTO "Econ" (employer_name, employer_website, job_title, job_apply_link, employer_logo, job_is_remote, job_city, job_state, job_posted_at_datetime_utc) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+//                     [employer_name, employer_website, job_title, job_apply_link, employer_logo, job_is_remote, job_city, job_state, job_posted_at_datetime_utc]);
+//             }
+//         }
+//         res.status(200).send('Jobs saved successfully');
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Server error');
+//     }
+// });
 
 app.get('/software-engineering-jobs', async (req, res) => {
     try {
@@ -136,6 +136,18 @@ app.get('/business-jobs', async (req, res) => {
             `SELECT * FROM "Business" ORDER BY job_posted_at_datetime_utc DESC`,
         )
         res.json(businessJobs.rows)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('Server error')
+    }
+})
+
+app.get('/econ-jobs', async (req, res) => {
+    try {
+        const econJobs = await pool.query(
+            `SELECT * FROM "Econ" ORDER BY job_posted_at_datetime_utc DESC`,
+        )
+        res.json(econJobs.rows)
     } catch (error) {
         console.error(error)
         res.status(500).send('Server error')
