42d83ecea3f9d4142a490b5a85259271189d8111
sethbehar <sethbehar@gmail.com>
2024-02-06 21:13:57 -0500

    User settings

--- internship-portal/src/pages/UserSettings.jsx
+++ internship-portal/src/pages/UserSettings.jsx
@@ -2,22 +2,41 @@ import React from 'react';
 import Nav from '../components/Nav';
 import './UserSettings.css';
 import { IoMdSettings } from "react-icons/io";
+import { UserAuth } from '../context/AuthContext';
 
 const UserSettings = () => {
+
+  const { user } = UserAuth();
+
   return (
-    <div className='user-settings'>
-      <header>
+    user ? (
+      <div className='user-settings'>
+        <header>
           <Nav />
-      </header>
-      <div className="setting-card">
-        <div className="settings-container">
-          <div className="row">
-            <h1>Settings</h1>
-            <IoMdSettings className='setting-icon'/>
+        </header>
+        <div className="setting-card">
+          <div className="settings-container">
+            <div className="row">
+              <h1>Settings</h1>
+              <IoMdSettings className='setting-icon' />
+            </div>
+            <div className="row">
+              <h2>Name: <span style={{ color: '#327ee2' }}>{user.displayName}</span></h2>
+            </div>
+            <div className="row">
+              <h2>Email: <span style={{ color: '#327ee2' }}>{user.email}</span></h2>
+            </div>
+            <div className='row'>
+              <button>Cancel Subscription</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
+    :
+    (
+      <div style={{fontSize:"4rem", color: 'white'}}>Redirect to home page</div>
+    )
   )
 }
 
