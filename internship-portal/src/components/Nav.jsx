7ce2cc811673f594225b24fdeb1778ddec2c7004
jakemoss127 <jacobmoss@ufl.edu>
2024-02-07 22:12:09 -0500

    Fixed pricing page

--- internship-portal/src/components/Nav.jsx
+++ internship-portal/src/components/Nav.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
-import {useState} from 'react';
-import { motion, useScroll, useMotionValueEvent } from 'framer-motion';
+import React from 'react';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 import './Nav.css';
 import internlogo from '../assets/internshark.svg';
 import Auth from '../pages/Auth';
 import { UserAuth } from '../context/AuthContext';
 import { CgProfile } from "react-icons/cg";
+import { Link } from 'react-router-dom';
 
-const Nav = () => { 
-
-  const {user, logOut} = UserAuth();
+const Nav = () => {
+  const { user, logOut } = UserAuth();
   const [showSettings, setShowSettings] = useState(false);
 
   return (
@@ -17,27 +17,33 @@ const Nav = () => {
       <motion.div className='nav-border'
       initial={{y: -100}}
       animate={{y: 0}}
-      transition={{duration: 1, ease: 'easeInOut'}}
-      >
-        <div className='logo-container'>
-          <img src={internlogo} alt="Internshark Logo" className="logo" style={{maxHeight: '25px', maxWidth: '25px'}}/>
-          <h1 className="logo-text">Internshark</h1>
-          <div className='pill'>Beta</div>
-        </div>
+      transition={{duration: 1, ease: 'easeInOut'}}>
+        <Link to='/' style={{textDecoration: 'none'}}>
+          <div className='logo-container'>
+            <img src={internlogo} alt="Internshark Logo" className="logo" style={{maxHeight: '25px', maxWidth: '25px'}}/>
+            <h1 className="logo-text">Internshark</h1>
+          </div>
+        </Link>
         <div className="middle-menu">
           <nav className="navbar">
-            <a href="/">Home</a>
-            <a href='/pricing'>Pricing</a>
-            <a href="https://github.com/jakemoss127/Internshark" target='_blank'>GitHub</a>
-            <a href='/about'>About</a>
-            <a href="/chart">Chart</a>
+            <Link to="/">Home</Link>
+            <Link to="/pricing">Pricing</Link>
+            <a href="https://github.com/jakemoss127/Internshark" target='_blank' rel="noopener noreferrer">GitHub</a>
+            <Link to="/about">About</Link>
+            <Link to="/chart">Chart</Link>
           </nav>
         </div>
         <div className="authentication-menu">
-          {!user ? <a></a> : <a href='/settings' style={{
-            display: 'flex', alignItems: 'center', textAlign: 'center', justifyContent: 'center', cursor: 'pointer'
-          }}><CgProfile style={{marginRight: '0.5rem', color: 'white', minHeight: '20px', minWidth: '20px'}}/>{user.displayName}</a>}
-          {!user && <Auth />}
+          {user ? (
+            <Link to='/settings' style={{
+              display: 'flex', alignItems: 'center', textAlign: 'center', justifyContent: 'center', cursor: 'pointer'
+            }}>
+              <CgProfile style={{marginRight: '0.5rem', color: 'white', minHeight: '20px', minWidth: '20px'}}/>
+              {user.displayName}
+            </Link>
+          ) : (
+            <Auth />
+          )}
           {user && <button className='sign-up' onClick={logOut}>Sign out</button>}
         </div>
       </motion.div>
@@ -45,4 +51,4 @@ const Nav = () => {
   )
 }
 
-export default Nav
+export default Nav;
--- internship-portal/src/pages/Pricing.jsx
+++ internship-portal/src/pages/Pricing.jsx
@@ -2,17 +2,12 @@ import React, { useEffect } from 'react'
 import Nav from '../components/Nav.jsx';
 import './Pricing.css';
 import PriceCard from '../components/PriceCard.jsx';
-import { UserAuth } from '../context/AuthContext';
 
 
 const Pricing = () => {
-
-    const { user } = UserAuth();
-
-    return (
-        user ? (
+    return (<>
+            <Nav />
             <div className='page-container'>
-                <header className='header'><Nav /></header>
                 <div className="content">
                     <div className="bubble">
                         <p className='pricing'>Pricing</p>
@@ -62,23 +57,7 @@ const Pricing = () => {
                     </div>
                 </div>
             </div>
-        ) : (
-            <div className='page-container'>
-                <header className='header'><Nav /></header>
-                <div className="content">
-                    <div className="bubble">
-                        <p className='pricing'>Pricing</p>
-                    </div>
-                    <div className="pricing-title">
-                        <h1>Choose the plan that fits<br /><span className='your'>your</span> needs.</h1>
-                        <h2 className="subtitle">Internshark can be used for free. Pro members gain access to additional features<br />and premium resources, unlocking the full potential of Internshark. </h2>
-                    </div>
-                    <div style={{color: 'blue', fontSize:'2rem'}}>
-                        Please sign in to view our prices
-                    </div>
-                </div>    
-            </div> 
-        )
+        </>
     )
                     
 }
